e204a75a01776a932e65d494e75c0274b12ab602
chris-breum <36517@edu.campusvejle.dk>
2024-05-29 09:21:01 +0200

    Maui build

---
+++ SoUs.CareApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SoUs.CareApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SoUs.CareApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SoUs.CareApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
